9fbb9920cac2e2fbbc66239c0be45d6c028d9ffb
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-07-31 17:57:34 +0200

    install firebase and add config file - app initialised and authorisation imported

---
+++ src/config/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKhe05QKeOgRi6vjxwIM6p5b5YoDeYw0c',
+  authDomain: 'maptheheat.firebaseapp.com',
+  projectId: 'maptheheat',
+  storageBucket: 'maptheheat.appspot.com',
+  messagingSenderId: '450743103808',
+  appId: '1:450743103808:web:438724886bff601b588aa5',
+  measurementId: 'G-GD5CWT0LJM',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
