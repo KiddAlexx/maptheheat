3923e080aa203e13b3f2b6a05c6ae9f4ebb6cfc1
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-03 15:53:00 +0200

    remove firebase test functions from AppLayout and import ListView component (currently emmpty)

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,75 +1,10 @@
-import { db } from '../config/firebase-config';
-import {
-  getDocs,
-  collection,
-  addDoc,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from 'firebase/firestore';
-
-import { useState, useEffect } from 'react';
-
-const restaurantCollectionRef = collection(db, 'restaurant-details');
+import ListView from '../components/ListView';
 
 function AppLayout() {
-  const [restaurantList, setRestaurantList] = useState([]);
-  console.log(restaurantList);
-
-  const [newRestaurantName, setNewRestaurantName] = useState('');
-  const [newRestaurantAddress, setNewRestaurantAddress] = useState('');
-
-  useEffect(() => {
-    const getRestaurantList = async () => {
-      try {
-        const data = await getDocs(restaurantCollectionRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setRestaurantList(filteredData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getRestaurantList();
-  }, []);
-
-  const onSubmitRestaurant = async (e) => {
-    e.preventDefault();
-    try {
-      await addDoc(restaurantCollectionRef, {
-        restaurantName: newRestaurantName,
-        restaurantAddress: newRestaurantAddress,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const deleteRestaurant = async (id) => {
-    const restaurantDoc = doc(db, 'restaurant-details', id);
-    await deleteDoc(restaurantDoc);
-  };
-  const updateRestaurant = async (id) => {
-    const restaurantDoc = doc(db, 'restaurant-details', id);
-    await updateDoc(restaurantDoc, { restaurantName: updatedRestaurantName });
-  };
-
   return (
-    <form onSubmit={onSubmitRestaurant}>
-      <input
-        type="text"
-        placeholder="name"
-        onChange={(e) => setNewRestaurantName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="address"
-        onChange={(e) => setNewRestaurantAddress(e.target.value)}
-      />
-      <button>submit</button>
-    </form>
+    <main>
+      <ListView></ListView>
+    </main>
   );
 }
 
