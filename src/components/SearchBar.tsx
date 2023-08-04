038830738d80cf75ead350606c953103a5010066
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-04 16:28:10 +0200

    add SearchBar component - no functionality + apply temp color to default button style

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+
 function SearchBar() {
-  return <form></form>;
+  const [searchValue, setSearchValue] = useState('');
+  console.log(searchValue);
+  return (
+    <form>
+      <input
+        type="text"
+        name="searchValue"
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <button type="submit" className="btn-default">
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default SearchBar;
--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,9 +1,11 @@
 import ListView from '../components/ListView';
+import SearchBar from '../components/SearchBar';
 import VenueForm from '../components/VenueForm';
 
 function AppLayout() {
   return (
     <main>
+      <SearchBar />
       <VenueForm />
       <ListView />
     </main>
