73a5084357951d30b27524ed7351183ba4d36130
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-16 18:47:47 +0200

    change map tile layer

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -47,7 +47,7 @@ function MapView() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
         />
         {restaurants.map((restaurant) => (
           <Marker
