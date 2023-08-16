32f34cbcec540e3511c695ecc4df89144b0064e4
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-16 17:58:51 +0200

    add some spacing to list items

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -11,9 +11,12 @@ function ListItem({ restaurant, handleClick }) {
       <div>
         <h2>{name}</h2>
         <h3>{address}</h3>
-        <p>{hours}</p>
+        <p>Open</p> {/* Temp , calculate open state based on hours */}
         <p>{phoneNumber}</p>
-        <Link to={`/app/venue/${city}/${urlSlug}`}>
+        <Link
+          className={styles.moreInfoLink}
+          to={`/app/venue/${city}/${urlSlug}`}
+        >
           <p
             onClick={() => {
               setActiveRestaurant(restaurant);
