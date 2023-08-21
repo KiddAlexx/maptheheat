26646b3b5afe890ee07d40cf224512390391e3b0
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-21 13:24:36 +0200

    add basic layout to VenueForm

--- src/components/VenueForm.tsx
+++ src/components/VenueForm.tsx
@@ -5,6 +5,8 @@ import { auth } from '../config/firebase-config';
 import slugify from 'slugify';
 import { VenueFormProps } from './SideBar';
 
+import styles from './VenueForm.module.css';
+
 function VenueForm({ setIsAddingVenue }: VenueFormProps) {
   // Data to be used for new venue entry
   const [venueData, setVenueData] = useState({
@@ -108,84 +110,100 @@ function VenueForm({ setIsAddingVenue }: VenueFormProps) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="city">City</label>
-        <select name="city" onChange={handleChange} value={city} id="city">
-          <option value="">Choose City</option>
-          <option value="Barcelona">Barcelona</option>
-          <option value="Madrid">Madrid</option>
-          <option value="Glasgow">Glasgow</option>
-          <option value="Edinburgh">Edinburgh</option>
-          <option value="London">London</option>
-        </select>
-        <label htmlFor="venueName">Restaurant Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={name}
-          id="venueName"
-        />
-        <label htmlFor="address">
-          Address - number followed by street name
-        </label>
-        <input
-          type="text"
-          name="address"
-          onChange={handleChange}
-          value={address}
-          id="address"
-        />
-        <label htmlFor="postcode">Postcode</label>
-        <input
-          type="text"
-          name="postcode"
-          onChange={handleChange}
-          value={postcode}
-          id="postcode"
-        />
-        <label htmlFor="description">Description</label>
-        <input
-          type="text"
-          name="description"
-          onChange={handleChange}
-          value={description}
-          id="description"
-        />
-        <label htmlFor="hours">Opening Hours</label>
-        <input
-          type="text"
-          name="hours"
-          onChange={handleChange}
-          value={hours}
-          id="hours"
-        />
-        <label htmlFor="phoneNumber">Phone Number</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          onChange={handleChange}
-          value={phoneNumber}
-          id="phoneNumber"
-        />
-        <label htmlFor="website">Website</label>
-        <input
-          type="text"
-          name="website"
-          onChange={handleChange}
-          value={website}
-          id="website"
-        />
-        <button type="submit" className="btn-default">
-          Submit
-        </button>
-        <button
-          onClick={() => setIsAddingVenue(false)}
-          type="button"
-          className="btn-default"
-        >
-          Cancel
-        </button>
+      <form onSubmit={handleSubmit} className={styles.venueFormContainer}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="city">City</label>
+          <select name="city" onChange={handleChange} value={city} id="city">
+            <option value="">Choose City</option>
+            <option value="Barcelona">Barcelona</option>
+            <option value="Madrid">Madrid</option>
+            <option value="Glasgow">Glasgow</option>
+            <option value="Edinburgh">Edinburgh</option>
+            <option value="London">London</option>
+          </select>
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="venueName">Restaurant Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={name}
+            id="venueName"
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="address">Address - Number / Street Name</label>
+          <input
+            type="text"
+            name="address"
+            onChange={handleChange}
+            value={address}
+            id="address"
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="postcode">Postcode</label>
+          <input
+            type="text"
+            name="postcode"
+            onChange={handleChange}
+            value={postcode}
+            id="postcode"
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            rows="2"
+            name="description"
+            onChange={handleChange}
+            value={description}
+            id="description"
+          ></textarea>
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="hours">Opening Hours</label>
+          <input
+            type="text"
+            name="hours"
+            onChange={handleChange}
+            value={hours}
+            id="hours"
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="text"
+            name="phoneNumber"
+            onChange={handleChange}
+            value={phoneNumber}
+            id="phoneNumber"
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="website">Website</label>
+          <input
+            type="text"
+            name="website"
+            onChange={handleChange}
+            value={website}
+            id="website"
+          />
+        </div>
+        <div className={styles.venueButtonContainer}>
+          <button
+            onClick={() => setIsAddingVenue(false)}
+            type="button"
+            className={`btn-default ${styles.btnCancel}`}
+          >
+            Cancel
+          </button>
+          <button type="submit" className={`btn-default ${styles.btnSubmit}`}>
+            Submit
+          </button>
+        </div>
       </form>
     </>
   );
