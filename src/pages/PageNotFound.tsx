fcd749027870a43727935ed26df839e1b94e9608
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-22 11:17:54 +0200

    add broken chilli image to PageNotFound

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,6 +1,14 @@
+import styles from './PageNotFound.module.css';
+import brokenChilli from '../assets/broken-chilli-grey-md.png';
+
 function PageNotFound() {
   return (
-    <div>
+    <div className={styles.pageContainer}>
+      <img
+        className={styles.brokenChilliImg}
+        src={brokenChilli}
+        alt="grey image of a broken chilli pepper"
+      />
       <h1>Woops, it looks like that page does not exist!🤔</h1>
     </div>
   );
