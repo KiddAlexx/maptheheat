657b4f2a70ad3432dc85e9d2878120875d7e7dca
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-08-24 12:08:25 +0200

    add comments to SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,12 @@
+// React imports
 import { useState } from 'react';
+
+// Component imports
 import ListView from '../components/ListView';
 import SearchBar from '../components/SearchBar';
 import VenueForm from '../components/VenueForm';
+
+// Style imports
 import styles from './SideBar.module.css';
 
 export interface VenueFormProps {
@@ -9,8 +14,10 @@ export interface VenueFormProps {
 }
 
 function SideBar() {
+  // State used to conditionally render VenueForm when true and ListView when false
+  // Value set in VenueForm and SearchBar
   const [isAddingVenue, setIsAddingVenue] = useState(false);
-  console.log(isAddingVenue);
+
   return (
     <div className={styles.sideBar}>
       <SearchBar setIsAddingVenue={setIsAddingVenue} />
