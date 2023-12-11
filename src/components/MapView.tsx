a3e8311dbe05d66e335dfc8214843c721fcf64a2
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-12-11 11:32:24 +0100

    remove isLoading state from MapView + add optional chaining to restaurant mapping incase of null values

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -21,11 +21,7 @@ function MapView() {
   const { activeRestaurant } = useRestaurantsContext();
 
   // Load restaurants from supabase
-  const {
-    restaurants,
-    error,
-    isLoading: isLoadingRestaurants,
-  } = useRestaurants();
+  const { restaurants, error } = useRestaurants();
 
   // Sets coordinates based on active restaurant value if available
   // Used to center map on active restaurant
@@ -64,7 +60,7 @@ function MapView() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
         />
-        {restaurants.map((restaurant) => (
+        {restaurants?.map((restaurant) => (
           <Marker
             key={restaurant.id}
             position={[restaurant.coords.lat, restaurant.coords.lon]}
