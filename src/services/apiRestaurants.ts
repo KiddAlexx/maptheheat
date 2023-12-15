94142fffe00a53fbc31589e799ca12a561d39b11
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-12-15 12:03:50 +0100

    handle image upload for restaurants: compress, upload to Supabase, and update DB entry

--- src/services/apiRestaurants.ts
+++ src/services/apiRestaurants.ts
@@ -1,5 +1,7 @@
 import { NewRestaurant } from '../models/restaurantTypes';
-import supabase from './supabase';
+import compressImage from '../utils/compressImage';
+import supabase, { supabaseUrl } from './supabase';
+import uploadImage from './supabaseImageUploader';
 
 export async function getRestaurants() {
   const { data, error } = await supabase.from('restaurant-details').select('*');
@@ -22,5 +24,37 @@ export async function createRestaurant(newRestaurant: NewRestaurant) {
     console.error('Supabase error:', error);
     throw new Error(`Restaurant could not be created. Error:${error.message}`);
   }
+  return data;
+}
+
+export async function createRestaurantImage(
+  restaurantId,
+  imgFile,
+  city,
+  restaurantName
+) {
+  // Compress image
+  const compressedImage = await compressImage(imgFile);
+
+  // Upload image to supabase bucket + return path
+  const imagePath = await uploadImage(
+    compressedImage,
+    'restaurant-images',
+    city,
+    restaurantName
+  );
+
+  const altText = `An image of ${restaurantName} in ${city}`;
+  const imageUrl = `${supabaseUrl}/storage/v1/object/public/${imagePath}`;
+
+  const { data, error } = await supabase
+    .from('restaurant-details')
+    .update([{ images: { alt: altText, url: imageUrl } }])
+    .eq('id', restaurantId);
+
+  if (error) {
+    throw new Error(`Error adding image to database: ${error.message}`);
+  }
+
   return data;
 }
