99ca4777af967e9a29b609838c4869923eb69654
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-02-05 09:46:43 +0100

    navigate to login page when user signs up for a new account

--- src/features/authentication/useEmailSignup.ts
+++ src/features/authentication/useEmailSignup.ts
@@ -1,14 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 import { signupApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 export function useEmailSignup() {
+  const navigate = useNavigate();
   const { mutate: signupEmail, isPending } = useMutation({
     mutationFn: signupApi,
     onSuccess: () => {
       toast.success(
         'Account successfully created! Please check your emails and verify your account'
       );
+      navigate('/login', { replace: true });
     },
     onError: (err) => {
       if (err instanceof Error) {
