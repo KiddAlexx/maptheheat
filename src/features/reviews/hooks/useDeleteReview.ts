89c375ee317bcbc2ea144f22ceb628bbdaa0efe0
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-02-20 09:02:23 +0100

    create useDeleteReview hook - utilises react query to call deleteReview function

---
+++ src/features/reviews/hooks/useDeleteReview.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteReview as deleteReviewApi } from '../../../services/apiReviews';
+import toast from 'react-hot-toast';
+
+export function useDeleteReview() {
+  const queryClient = useQueryClient();
+
+  const { isPending: isDeleting, mutate: deletReview } = useMutation({
+    mutationFn: deleteReviewApi,
+    onSuccess: () => {
+      toast.success('Review successfully deleted');
+      queryClient.invalidateQueries({ queryKey: ['reviews'] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+  return { isDeleting, deletReview };
+}
