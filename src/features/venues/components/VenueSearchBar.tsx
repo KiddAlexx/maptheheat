345cd21e6d73c06fa3f001738a788395052ef5cf
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-05-12 15:11:19 +0200

    use uniqueCities in VenuSearchBar to populate auto complete list

--- src/features/venues/components/VenueSearchBar.tsx
+++ src/features/venues/components/VenueSearchBar.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import styles from '../styles/SearchBar.module.css';
 import { Autocomplete, AutocompleteItem } from '@nextui-org/react';
 import { useVenueFilterContext } from '@/context/VenueFilterContext';
-import { getUniqueCities } from '@/services/apiVenues';
+import { useUniqueCities } from '../hooks/useUniqueCities';
+import LoaderSpinner from '@/ui/LoaderSpinner';
 
 function VenueSearchBar() {
-  const cities = ['Berlin', 'Barcelona'];
-
   const [selectedCity, setSelectedCity] = useState('');
+
+  const { uniqueCities, error, isPending: isPendingCities } = useUniqueCities();
 
   const handleSelectCity = (value) => {
     setSelectedCity(value);
@@ -19,7 +20,9 @@ function VenueSearchBar() {
     updateVenueFilter({ field: 'city', value: selectedCity, method: 'eq' });
   }
 
-  return (
+  return isPendingCities ? (
+    <LoaderSpinner />
+  ) : (
     <form onSubmit={handleSubmit}>
       <Autocomplete
         placeholder="Search by city"
@@ -29,7 +32,7 @@ function VenueSearchBar() {
         onInputChange={handleSelectCity}
         onSelectionChange={handleSelectCity}
       >
-        {cities.map((city, index) => (
+        {uniqueCities!.map((city, index) => (
           // Using city + index as key for uniqueness in case of duplicate city names
           <AutocompleteItem key={`${city}-${index}`}>{city}</AutocompleteItem>
         ))}
