ddb0585f84d3f92c946f3f9d52e5949ca764e7fe
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-05-13 12:12:31 +0200

    add basic styling to VenueTypeFilter

--- src/features/venues/components/VenueTypeFilter.tsx
+++ src/features/venues/components/VenueTypeFilter.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonGroup } from '@nextui-org/react';
 import { useState } from 'react';
 
 function VenueTypeFilter() {
-  const [activeType, setActiveType] = useState('All');
+  const [activeType, setActiveType] = useState('all');
   const { updateVenueFilter, removeVenueFilter, filters } =
     useVenueFilterContext();
 
@@ -17,12 +17,28 @@ function VenueTypeFilter() {
     console.log(filters);
   }
   return (
-    <ButtonGroup>
-      <Button onPress={() => handleFilterClick('all')}>All</Button>
-      <Button onPress={() => handleFilterClick('restaurant')}>
+    <ButtonGroup radius="sm">
+      <Button
+        color={activeType === 'all' ? 'primary' : 'default'}
+        isDisabled={activeType === 'all'}
+        onPress={() => handleFilterClick('all')}
+      >
+        All
+      </Button>
+      <Button
+        color={activeType === 'restaurant' ? 'primary' : 'default'}
+        isDisabled={activeType === 'restaurant'}
+        onPress={() => handleFilterClick('restaurant')}
+      >
         Restaurants
       </Button>
-      <Button onPress={() => handleFilterClick('shop')}>Shops</Button>
+      <Button
+        color={activeType === 'shop' ? 'primary' : 'default'}
+        isDisabled={activeType === 'shop'}
+        onPress={() => handleFilterClick('shop')}
+      >
+        Shops
+      </Button>
     </ButtonGroup>
   );
 }
