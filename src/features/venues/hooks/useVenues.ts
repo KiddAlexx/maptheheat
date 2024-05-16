591f4fcc333eace949e39d2580a74bdd66771f95
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-05-16 12:24:57 +0200

    update useVenues query hook to accept sort object

--- src/features/venues/hooks/useVenues.ts
+++ src/features/venues/hooks/useVenues.ts
@@ -1,16 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { getVenues } from '../../../services/apiVenues';
 import { Venue } from '../../../models/venueTypes';
-import { VenueFilter } from '@/context/VenueFilterContext';
+import { VenueFilter, VenueSort } from '@/context/VenueFilterContext';
 
-export function useVenues(filters: VenueFilter[]) {
+export function useVenues(filters: VenueFilter[], sort: VenueSort) {
   const {
     error,
     isLoading,
     data: venues,
   } = useQuery<Venue[]>({
-    queryKey: ['venues', filters],
-    queryFn: () => getVenues(filters),
+    queryKey: ['venues', filters, sort],
+    queryFn: () => getVenues(filters, sort),
   });
   return { error, isLoading, venues };
 }
