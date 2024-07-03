bc87d6508be2feb088724d783e1738a24da214ae
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-07-03 17:11:16 +0200

    update useVenues query hook to return both venues count

--- src/features/venues/hooks/useVenues.ts
+++ src/features/venues/hooks/useVenues.ts
@@ -1,16 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import { getVenues } from '../../../services/apiVenues';
-import { Venue } from '../../../models/venueTypes';
+import { VenuesResponse, getVenues } from '../../../services/apiVenues';
 import { VenueFilter, VenueSort } from '@/context/VenueFilterContext';
 
 export function useVenues(filters: VenueFilter[], sort?: VenueSort | null) {
-  const {
-    error,
-    isLoading,
-    data: venues,
-  } = useQuery<Venue[]>({
+  const { error, isLoading, data } = useQuery<VenuesResponse>({
     queryKey: ['venues', filters, sort],
     queryFn: () => getVenues(filters, sort),
   });
-  return { error, isLoading, venues };
+
+  const venues = data?.data;
+  const totalCount = data?.count;
+  return { error, isLoading, venues, totalCount };
 }
