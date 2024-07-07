f2b20310b497366d794555defe856cf3033d6f1e
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-07-07 08:13:35 +0200

    wrap app with ReviewSortProvider + create basic ReviewSort component

--- src/features/reviews/components/ReviewSort.tsx
+++ src/features/reviews/components/ReviewSort.tsx
@@ -1,5 +1,61 @@
+import {
+  ReviewSortField,
+  useReviewSortContext,
+} from '@/context/ReviewSortContext';
+import { Direction } from '@/context/VenueFilterContext';
+import { Select, SelectItem } from '@nextui-org/react';
+import { FaSortAmountDownAlt, FaSortAmountUp } from 'react-icons/fa';
+
 function ReviewSort() {
-  return <div>Review Sort</div>;
+  const { updateSort, resetSort } = useReviewSortContext();
+
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value === 'default') {
+      resetSort();
+    } else {
+      const [field, direction] = e.target.value.split('-');
+      // Types asserted due to TypeScript interpreting event value as a string
+      updateSort({
+        field: field as ReviewSortField,
+        direction: direction as Direction,
+      });
+    }
+  }
+  return (
+    <Select onChange={handleChange} label={'Sort by'}>
+      <SelectItem value={'default'} key={'default'}>
+        Default
+      </SelectItem>
+      <SelectItem
+        endContent={<FaSortAmountUp />}
+        value={'averageRating-desc'}
+        key={'averageRating-desc'}
+      >
+        Heat Rating
+      </SelectItem>
+      <SelectItem
+        endContent={<FaSortAmountDownAlt />}
+        value={'averageRating-asc'}
+        key={'averageRating-asc'}
+      >
+        Heat Rating
+      </SelectItem>
+      <SelectItem
+        endContent={<FaSortAmountUp />}
+        value={'createdAt-desc'}
+        key={'createdAt-desc'}
+      >
+        Date Added
+      </SelectItem>
+      <SelectItem
+        endContent={<FaSortAmountDownAlt />}
+        value={'createdAt-asc'}
+        key={'createdAt-asc'}
+      >
+        Date Added
+      </SelectItem>
+    </Select>
+  );
 }
 
 export default ReviewSort;
