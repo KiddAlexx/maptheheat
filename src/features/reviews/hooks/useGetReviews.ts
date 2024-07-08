6b5eb6890294f17bbfef00836415412089d5dd47
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-07-08 12:07:00 +0200

    update useGetReviews query hook to accept sort and pagination arguments and return totalCount of returned results

--- src/features/reviews/hooks/useGetReviews.ts
+++ src/features/reviews/hooks/useGetReviews.ts
@@ -1,16 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import { getReviews } from '../../../services/apiReviews';
-import { Review } from '../../../models/reviewTypes';
+import { getReviews, ReviewsResponse } from '../../../services/apiReviews';
+import { ReviewPagination, ReviewSort } from '@/context/ReviewSortContext';
 
-export function useGetReviews(venueId: string) {
-  const {
-    isLoading,
-    data: reviews,
-    error,
-  } = useQuery<Review[]>({
-    queryKey: ['reviews', venueId],
-    queryFn: () => getReviews(venueId),
+export function useGetReviews(
+  venueId: string,
+  sort?: ReviewSort,
+  pagination?: ReviewPagination
+) {
+  const { isLoading, data, error } = useQuery<ReviewsResponse>({
+    queryKey: ['reviews', venueId, sort, pagination],
+    queryFn: () => getReviews(venueId, sort, pagination),
   });
 
-  return { isLoading, error, reviews };
+  const reviews = data?.data;
+  const totalCount = data?.count;
+  return { error, isLoading, reviews, totalCount };
 }
