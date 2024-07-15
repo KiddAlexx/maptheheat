eb3a4b78e7103c80d4b9827df59ab29912003124
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-07-15 22:24:47 +0200

    import Tabs component from NextUI to use in UserProfile - tab for reviews and for favourite venues

--- src/features/userProfile/components/UserProfile.tsx
+++ src/features/userProfile/components/UserProfile.tsx
@@ -3,12 +3,15 @@ import ReviewContainer from '@/features/reviews/components/ReviewContainer';
 import VenueListContainer from '@/features/venues/components/VenueListContainer';
 import { useGetUserProfile } from '../hooks/useGetUserProfile';
 import LoaderSpinner from '@/ui/LoaderSpinner';
+import { Tab, Tabs } from '@nextui-org/react';
+import { useState } from 'react';
 
 function UserProfile() {
   const { user, isLoading: isLoadingUser, fetchStatus } = useUser();
   const { id: userId } = user;
   const { userProfile, isLoading: isLoadingProfile } =
     useGetUserProfile(userId);
+  const [selected, setSelected] = useState('reviews');
 
   if (fetchStatus == 'fetching' || isLoadingUser || isLoadingProfile) return;
   <LoaderSpinner />;
@@ -16,14 +19,20 @@ function UserProfile() {
   const { updatedAt, username, avatarUrl, totalReviews, favouriteVenues } =
     userProfile;
 
-  console.log(user);
-  console.log(userId);
-  console.log(userProfile);
-
   return (
     <div>
-      <ReviewContainer mode="user" />
-      <VenueListContainer mode="user" favouriteVenues={favouriteVenues} />
+      <Tabs
+        aria-label="Options"
+        selectedKey={selected}
+        onSelectionChange={setSelected}
+      >
+        <Tab key={'reviews'} title="My Reviews">
+          <ReviewContainer mode="user" />
+        </Tab>
+        <Tab key={'venues'} title="Favourite Venues">
+          <VenueListContainer mode="user" favouriteVenues={favouriteVenues} />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
