de35f0144f681bbe0334140ae8be8d6e61d2f87c
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-08-02 10:32:39 +0200

    update navigate hook in handleReview function to direct user to username section of profile if they have not chosen a username

--- src/features/venues/components/DetailedVenueView.tsx
+++ src/features/venues/components/DetailedVenueView.tsx
@@ -99,7 +99,7 @@ function DetailedVenueView() {
     }
     if (!username) {
       openDialog('Please choose a username to proceed', () =>
-        navigate(`/profile/edit`)
+        navigate(`/profile/edit/username`)
       );
       return;
     }
