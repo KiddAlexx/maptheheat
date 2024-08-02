95562518049f5ed5b1a19a8c02942f5d789eacbe
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-08-02 07:52:23 +0200

    adjust text for reviews created + venues added in UserProfileBanner

--- src/context/ReviewSortContext.tsx
+++ src/context/ReviewSortContext.tsx
@@ -32,7 +32,7 @@ const ReviewSortContext = createContext<ReviewFilterContextType | undefined>(
 
 const initialState: State = {
   sort: null,
-  pagination: { pageNumber: 1, maxResults: 1 },
+  pagination: { pageNumber: 1, maxResults: 5 },
 };
 
 function reducer(state: State, action: Action) {
