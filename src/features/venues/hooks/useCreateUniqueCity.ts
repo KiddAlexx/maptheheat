6be991e614137e09f3bc0c218e1694c57687fc38
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2024-08-08 12:50:38 +0200

    add missing paramater to useCreateUniqueCity hook

--- src/features/venues/hooks/useCreateUniqueCity.ts
+++ src/features/venues/hooks/useCreateUniqueCity.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { createUniqueCityApi } from '../../../services/apiVenues';
 
-export function useCreateUniqueCity() {
+export function useCreateUniqueCity({ cityObj }) {
   const { mutateAsync: createUniqueCity, isPending: isCreating } = useMutation({
     mutationFn: createUniqueCityApi,
   });
