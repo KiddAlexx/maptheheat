ba53455e1b77e7058e54aa8604486c75f4a2df6d
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-08-30 12:19:26 +0200

    chore(notifications): add query function to update user notification

---
+++ src/features/userProfile/hooks/useUpdateUserNotification.ts
@@ -0,0 +1,25 @@
+import { updateUserNotificationApi } from '@/services/apiUserProfiles';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+interface UpdateUserNotificationArgs {
+  notificationId: string;
+}
+
+export function useUpdateUserNotification() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUserNotification, isPending: isUpdating } = useMutation(
+    {
+      mutationFn: ({ notificationId }: UpdateUserNotificationArgs) =>
+        updateUserNotificationApi({ notificationId }),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['notifications'] });
+      },
+      onError: (err) => {
+        toast.error(err.message);
+      },
+    }
+  );
+  return { isUpdating, updateUserNotification };
+}
