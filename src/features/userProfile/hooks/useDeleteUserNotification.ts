57eb772c1d9c5bbcaab114033a28a38575700385
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-08-30 12:08:38 +0200

    chore(notifications): add query function to delete user notification

---
+++ src/features/userProfile/hooks/useDeleteUserNotification.ts
@@ -0,0 +1,25 @@
+import { deleteUserNotificationApi } from '@/services/apiUserProfiles';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+interface DeleteUserNotificationArgs {
+  notificationId: string;
+}
+
+export function useDeleteUserNotification() {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteUserNotification, isPending: isDeleting } = useMutation(
+    {
+      mutationFn: ({ notificationId }: DeleteUserNotificationArgs) =>
+        deleteUserNotificationApi({ notificationId }),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['notifications'] });
+      },
+      onError: (err) => {
+        toast.error(err.message);
+      },
+    }
+  );
+  return { isDeleting, deleteUserNotification };
+}
