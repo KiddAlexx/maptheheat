8db2919b0964c6c38b2f8feb86110e4dfee4d5f9
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-09-01 19:01:44 +0200

    perf(pagination): implement pre fetching for reviews

--- src/features/reviews/hooks/useGetReviews.ts
+++ src/features/reviews/hooks/useGetReviews.ts
@@ -1,4 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import {
+  useQuery,
+  useQueryClient,
+  keepPreviousData,
+} from '@tanstack/react-query';
 import {
   getReviews,
   ReviewsRequestParams,
@@ -11,12 +15,35 @@ export function useGetReviews({
   sort,
   pagination,
 }: ReviewsRequestParams) {
+  const pageNumber = pagination?.pageNumber ?? 1;
+  const maxResults = pagination?.maxResults ?? 10;
+
+  const queryClient = useQueryClient();
+
   const { isLoading, data, error } = useQuery<ReviewsResponse>({
     queryKey: ['reviews', venueId, userId, sort, pagination],
     queryFn: () => getReviews({ venueId, userId, sort, pagination }),
+    placeholderData: keepPreviousData,
   });
 
   const reviews = data?.data;
   const totalCount = data?.count ?? 0;
+
+  const pageCount = Math.ceil(totalCount / maxResults);
+
+  if (pageNumber < pageCount) {
+    const next = { pageNumber: pageNumber + 1, maxResults };
+    queryClient.prefetchQuery({
+      queryKey: ['reviews', venueId, userId, sort, next],
+      queryFn: () => getReviews({ venueId, userId, sort, pagination: next }),
+    });
+  }
+  if (pageNumber > 1) {
+    const prev = { pageNumber: pageNumber - 1, maxResults };
+    queryClient.prefetchQuery({
+      queryKey: ['reviews', venueId, userId, sort, prev],
+      queryFn: () => getReviews({ venueId, userId, sort, pagination: prev }),
+    });
+  }
   return { error, isLoading, reviews, totalCount };
 }
