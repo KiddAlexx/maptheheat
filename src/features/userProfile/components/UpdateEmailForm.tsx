afcc713e2a4c6d7edde6da9ec67787d6d8abf108
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-09-03 14:07:21 +0200

    fix(update-email): convert update email form from register to controller

--- src/features/userProfile/components/UpdateEmailForm.tsx
+++ src/features/userProfile/components/UpdateEmailForm.tsx
@@ -1,6 +1,6 @@
 import { useUpdateEmail } from '@/features/authentication/hooks/useUpdateEmail';
 import { Button, Input } from '@heroui/react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 
 function UpdateEmailForm() {
   interface FormData {
@@ -9,7 +9,7 @@ function UpdateEmailForm() {
   }
 
   const { updateEmail } = useUpdateEmail();
-  const { register, formState, handleSubmit, getValues, reset } =
+  const { control, formState, handleSubmit, getValues, reset } =
     useForm<FormData>();
   const { errors } = formState;
 
@@ -25,46 +25,57 @@ function UpdateEmailForm() {
       onSubmit={handleSubmit(formSubmit)}
       className="w-full max-w-3xl"
     >
-      <Input
-        id="email"
-        type="email"
-        label="New Email"
-        radius="sm"
-        variant="bordered"
-        isInvalid={!!errors.email}
-        errorMessage={
-          errors.email && typeof errors?.email?.message === 'string'
-            ? errors.email.message
-            : ''
-        }
-        {...register('email', {
+      <Controller
+        name="email"
+        control={control}
+        rules={{
           required: 'This field is required',
           pattern: {
             value: /\S+@\S+\.\S+/,
             message: 'Please provide a valid email address',
           },
-        })}
+        }}
+        render={({ field }) => (
+          <Input
+            {...field}
+            id="email"
+            type="email"
+            label="Email"
+            radius="sm"
+            variant="bordered"
+            isInvalid={!!errors.email}
+            errorMessage={errors?.email?.message}
+          />
+        )}
       />
-      <Input
-        className="mt-2"
-        id="confirmEmail"
-        type="email"
-        label="Confirm Email"
-        radius="sm"
-        variant="bordered"
-        isInvalid={!!errors.confirmEmail}
-        errorMessage={
-          errors.confirmEmail &&
-          typeof errors?.confirmEmail?.message === 'string'
-            ? errors.confirmEmail.message
-            : ''
-        }
-        {...register('confirmEmail', {
+      <Controller
+        name="confirmEmail"
+        control={control}
+        rules={{
           required: 'This field is required',
           validate: (value) =>
             value === getValues().email || 'Email address does not match',
-        })}
+        }}
+        render={({ field }) => (
+          <Input
+            {...field}
+            className="mt-2"
+            id="confirmEmail"
+            type="email"
+            label="Confirm Email"
+            radius="sm"
+            variant="bordered"
+            isInvalid={!!errors.confirmEmail}
+            errorMessage={
+              errors.confirmEmail &&
+              typeof errors?.confirmEmail?.message === 'string'
+                ? errors.confirmEmail.message
+                : ''
+            }
+          />
+        )}
       />
+
       <div className="mt-2 flex justify-end">
         <Button radius="sm" size="md" type="submit">
           Update Email
