47bf17996e256eb8cccccb582b1626d88ec063a8
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-09-12 12:15:26 +0200

    feat(compression): add function to compress 3 versions of image, sm, md, lg

--- src/utils/compressImage.ts
+++ src/utils/compressImage.ts
@@ -8,6 +8,7 @@ interface CompressionOptions {
   fileType?: 'image/webp' | 'image/jpeg' | 'image/png' | 'image/avif';
 }
 
+// Function to compress image/s with one compression setting
 export async function compressImage(
   imageFiles: File[],
   compressionOptions?: CompressionOptions
@@ -38,4 +39,46 @@ export async function compressImage(
     }
   }
   return compressedImages;
+}
+
+// Function to compress image/s with multiple compression options
+
+const largeCompressionOptions = {
+  maxWidthOrHeight: 2400,
+  maxSizeMB: 1.2,
+  fileType: 'image/webp',
+  useWebWorker: true,
+};
+const mediumCompressionOptions = {
+  maxWidthOrHeight: 1280,
+  maxSizeMB: 0.5,
+  fileType: 'image/webp',
+  useWebWorker: true,
+};
+const smallCompressionOptions = {
+  maxWidthOrHeight: 400,
+  maxSizeMB: 0.08,
+  fileType: 'image/webp',
+  useWebWorker: true,
+};
+
+export async function compressImageVariants(imageFiles: File[]) {
+  const compressedImageBundle = [];
+  for (const imageFile of imageFiles) {
+    try {
+      const [lg, md, sm] = await Promise.all([
+        imageCompression(imageFile, largeCompressionOptions),
+        imageCompression(imageFile, mediumCompressionOptions),
+        imageCompression(imageFile, smallCompressionOptions),
+      ]);
+      compressedImageBundle.push({ variants: { lg, md, sm } });
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(
+          `There was an error compressing your image ${err.message}`
+        );
+      }
+    }
+  }
+  return compressedImageBundle;
 }
