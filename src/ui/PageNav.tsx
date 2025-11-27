ce2732dcdf75622a35d4317800d65107e9d90fb8
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2025-11-27 17:43:10 +0100

    feat(nav): restructure PageNav + improve accessibility

--- src/ui/PageNav.tsx
+++ src/ui/PageNav.tsx
@@ -16,29 +16,33 @@ function PageNav() {
   const { openModal } = useModalContext();
 
   return (
-    <nav className="flex h-16 items-center justify-between bg-zinc-700 px-6">
-      <h3 className={styles.logoMain}>
-        <NavLink to="/">MapTheHeat</NavLink>
-      </h3>
-
-      <ul className="flex items-center gap-4">
-        <div className="text-sm">
-          <MapButton />
-        </div>
-        <div className="text-sm">
-          <AddVenueButton />
-        </div>
+    <nav
+      aria-label="Main navigation"
+      className="flex h-16 items-center justify-between bg-zinc-700 px-8 "
+    >
+      <ul className="flex w-full items-center justify-between">
+        <li className={styles.logoMain}>
+          <NavLink to="/">MapTheHeat</NavLink>
+        </li>
+        <li>
+          <div className="flex items-center gap-5">
+            <MapButton />
+            <AddVenueButton />
+          </div>
+        </li>
 
         {/* Checks user login state
             Displays Login & Signup or Logout button */}
         {!isAuthenticated ? (
-          <>
-            <li>
-              <Button size="md" radius="sm" onPress={() => openModal('login')}>
-                Login / Sign Up
-              </Button>
-            </li>
-          </>
+          <li>
+            <Button
+              size="sm"
+              onPress={() => openModal('login')}
+              className="h-9 min-w-32 bg-success-300 text-base font-medium text-success-foreground "
+            >
+              Sign In
+            </Button>
+          </li>
         ) : (
           <li className="flex items-center">
             <UserMenu />
