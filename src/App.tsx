ec71cb8f196bddd71fce2ad6f3a3fa470f62f09b
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2026-01-26 10:02:04 +0100

    feat(toasts): give all toasts default role of alert + aria live assertive - temp fix as tole status + polite not being announced

--- src/App.tsx
+++ src/App.tsx
@@ -90,14 +90,20 @@ function App() {
       </div>
       <ErrorModal />
       <ModalManager />
-
       <Toaster
         position="top-center"
         gutter={12}
         containerStyle={{ margin: '8px' }}
         toastOptions={{
-          success: { duration: 3000 },
-          error: { duration: 5000 },
+          success: {
+            duration: 3000,
+            /* Temp make all toasts alert due to status + poilte not being announced */
+            ariaProps: { role: 'alert', 'aria-live': 'assertive' },
+          },
+          error: {
+            duration: 5000,
+            ariaProps: { role: 'alert', 'aria-live': 'assertive' },
+          },
           style: {
             fontSize: '16px',
             maxWidth: '500px',
