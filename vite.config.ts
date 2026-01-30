e15e25ae6005db90790f7fc3340cca2f5db1686c
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2026-01-30 17:05:52 +0100

    build(packages): install vitest, react testing library, jest/dom, jsdom + basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -9,5 +9,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'tests/setup.ts',
   },
 });
