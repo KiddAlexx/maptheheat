454515f12b0a40296acc4c4fde2a05c7c7db55a8
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2026-03-04 18:28:28 +0100

    feat(venue-list): implement scroll to top behavior when changing pages

--- src/features/venues/components/VenueListContainer.tsx
+++ src/features/venues/components/VenueListContainer.tsx
@@ -2,6 +2,7 @@ import { useVenueFilterContext } from '@/context/VenueFilterContext';
 import SearchAndFilterPanel from './SearchAndFilterPanel';
 import ListView from './VenueListView';
 import { useUserFavVenuesContext } from '@/context/UserFavVenuesContext';
+import { useLayoutEffect, useRef } from 'react';
 
 interface VenueListContainerProps {
   mode: 'venue' | 'user';
@@ -17,8 +18,19 @@ function VenueListContainer({
   const useVenueContext =
     mode === 'venue' ? useVenueFilterContext : useUserFavVenuesContext;
 
+  const { pagination } = useVenueContext();
+  const venueScrollRef = useRef<HTMLDivElement | null>(null);
+
+  useLayoutEffect(() => {
+    venueScrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pagination.pageNumber]);
+
   return (
-    <div className="h-full overflow-y-scroll [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+    <div
+      ref={venueScrollRef}
+      className="h-full overflow-y-scroll  [-ms-overflow-style:none]  [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+    >
       <div className="mb-2 rounded-xl bg-zinc-200 p-3">
         <SearchAndFilterPanel
           useVenueContext={useVenueContext}
