bced94e08d5759e01117362d6debace11b8a2f15
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2026-04-27 16:53:53 +0200

    style(footer): hide community message on small screens

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -5,7 +5,7 @@ function HomeFooter() {
     <footer className="flex h-14 items-center justify-between  bg-slate-950/80 px-4 sm:px-8">
       <div className="flex w-full items-center justify-between py-4 text-sm text-primary-50  sm:text-base">
         <span>© {new Date().getFullYear()} MapTheHeat</span>
-        <span className="hidden xs:block">Built by the community 🔥</span>
+        <span className="hidden sm:block">Built by the community 🔥</span>
 
         <div className="flex items-center gap-6">
           <Link to="/contact">Contact</Link>
