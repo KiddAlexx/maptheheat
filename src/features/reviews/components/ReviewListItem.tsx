b42e1c7387daec8041b0250258da02ddf36d9bb4
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2026-05-25 22:18:21 +0200

    docs(a11y): restore aria-label on heat rating wrapper in ReviewListItem

--- src/features/reviews/components/ReviewListItem.tsx
+++ src/features/reviews/components/ReviewListItem.tsx
@@ -126,6 +126,8 @@ function ReviewListItem({ review, mode }: ReviewListItemProps) {
             </div>
             <div className="mr-5 sm:mr-0">
               <div
+                role="group"
+                aria-label={`Review heat rating ${heatRating}`}
                 data-value={heatRating}
                 className=" -translate-x-[1px] sm:mt-2 [&>span]:!flex"
               >
